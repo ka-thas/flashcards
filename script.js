993429c7c319b2bbcfc6a4140c1a91a330022c60
Ka Thas <kavinthas@gmail.com>
2024-09-04 20:40:16 +0900

    Styling - Form no longer causes the entire page to jump when toggling visibility

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-const questionInput = document.getElementById('question');
-const answerInput = document.getElementById('answer');
+// Description: This script is for a flashcard app that allows users to add, delete, and view flashcards.
+
+// Variables
+// Card
 const questionText = document.getElementById('question-text');
 const answerText = document.getElementById('answer-text');
+// Menu
+const menuEl = document.getElementById('menu');
+const delBtn = document.getElementById('delete');
 const plusBtn = document.getElementById('add');
 const nextBtn = document.getElementById('next');
-const delBtn = document.getElementById('delete');
+// Form
 const formEl = document.getElementById('form');
+const questionInput = document.getElementById('question');
+const answerInput = document.getElementById('answer');
+const closeBtn = document.getElementById('close');
 const submitBtn = document.getElementById('submit');
-const allFlashcards = [];
+// Logic
+let allFlashcards = [];
 let currentFlashcardIndex = 0;
 
 
 main();
 
 function main() {
-    formEl.style.display = "none";
     
     // Event listeners
     delBtn.addEventListener('click', delete_flashcard);
-    plusBtn.addEventListener('click', () => formEl.style.display = "block");
+    plusBtn.addEventListener('click', show_form);
     nextBtn.addEventListener('click', pick_random_flashcard);
     submitBtn.addEventListener('click', add_flashcard);
+    closeBtn.addEventListener('click', hide_form);
 
 
-    if (!localStorage.getItem('flashcards')) {
 
-    allFlashcards = [
-        { question: "Who created Simula?", answer: "Kristen Nygård and Ole Johan Dahl" },
-        { question: "Hello in Korean", answer: "안녕하세요" },
-    ];
-        save_flashcards();
+    if (!localStorage.getItem('flashcards')) { // If there are no flashcards in local storage
+        allFlashcards = [
+            { question: "Who created Simula?", answer: "Kristen Nygård and Ole Johan Dahl" },
+            { question: "Hello in Korean", answer: "안녕하세요" },
+        ];
+            save_flashcards();
     }
     load_flashcards();
     console.log(allFlashcards);
@@ -69,6 +78,18 @@ function load_flashcards() {
     }
 }
 
+function show_form() {
+    formEl.style.display = "block";
+    menuEl.style.marginBottom = "5px";
+}
+
+function hide_form() {
+    // Clear the input fields
+    questionInput.value = '';
+    answerInput.value = '';
+    formEl.style.display = "none";
+    menuEl.style.marginBottom = "133px";
+}
 
 function add_flashcard() {
     const question = questionInput.value;
@@ -77,13 +98,6 @@ function add_flashcard() {
     allFlashcards.push({ question, answer });
 
     save_flashcards();
-
-    // Clear the input fields
-    questionInput.value = '';
-    answerInput.value = '';
-
-    formEl.style.display = "none";
-
 }
 
 function delete_flashcard() {
